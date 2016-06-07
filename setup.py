25df214627dadb85c8b6b17956753823fcc43669
Adam DePrince <adam@pelotoncycle.com>
2016-06-07 17:52:13 -0400

    Version 0.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='peloton_bloomfilters',
       author = 'Adam DePrince',
       author_email = 'adam@pelotoncycle.com',
       url = 'https://github.com/pelotoncycle/peloton_bloomfilters',
-      version='0.0.2',
+      version='0.0.1',
       description="Peloton Cycle's Bloomin fast Bloomfilters",
       ext_modules=(
           [
